b70183dcfec2178c70a0ffcdd72c6af137402258
andika <andikaalvierayp@gmail.com>
2024-01-15 14:48:56 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { CardImage, CardImageTitle, CardImageTitleDestination } from './component/CardImage'
+import { MidCard } from './component/MidCard'
+import { Footer } from './component/Footer'
+import InfoCard from './component/InfoCard'
+import SmoothTrip from './component/SmoothTrip'
+import SmoothTripInfo from './component/SmoothTripInfo'
+import About from './component/About'
+import FloatingActionButton from './component/FloatingActionButton'
+
+export default function Home() {
+  return (
+    <>
+
+    
+
+    <FloatingActionButton/>
+
+
+    
+    <CardImageTitle/>
+    <CardImage/>
+    {/* <CardImageTitleDestination/> */}
+
+    <MidCard/>
+
+    <About/>
+
+    <InfoCard/>
+    <SmoothTrip/>
+    <SmoothTripInfo/>
+
+    <Footer/>
+    </>
+  )
+}
